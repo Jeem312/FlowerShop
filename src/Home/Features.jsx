20d290dd4617136e161d1f652ec6ca1c16f06d74
Jeem312 <Shajidajeem312@gmail.com>
2025-02-08 22:02:19 +0600

    designed features section

--- src/Home/Features.jsx
+++ src/Home/Features.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
-
+import '../Styles/Features.css'
 const Features = () => {
     return (
-        <div>
+        <div className='Features'>
+       <div className='container'>
+       <div className='Text-div'>
+           <h2 className='heading'>Our Featured Flowers</h2>
+           <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+           </div>
+           <div className='flower-div'>
+          <div className='flowers'>
+          <div className='flowerContainer-1'>
+                <div>
+                    <img src="../Flower Asset/pngwing 6.png" alt="" />
+                </div>
+             
+            </div>
+            <p> Pink Flower Tree </p>
+          </div>
+           <div className="flowers">
+           <div className='flowerContainer-2'>
+                <div>
+                    <img src="../Flower Asset/pngwing 5.png" alt="" />
+                </div>
+        
+            </div>
             
+                <p> Red Flower Tree </p>
+            
+           </div>
+          <div className='flowers'>
+          <div className='flowerContainer-3'>
+                <div>
+                    <img src="../Flower Asset/pngwing 7.png" alt="" />
+                </div>
+                <p> Yellow Flower Tree </p>
+            </div>
+          </div>
+           </div>
+       </div>
         </div>
     );
 };
